3d165ee86455bd06c99316cd8f795803949e6b30
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-08-23 06:54:52 +0530

    Update index.ts

--- Food webpage/server/src/index.ts
+++ Food webpage/server/src/index.ts
@@ -5,7 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
 app.use("/images", express.static(path.join(__dirname, "../public/images")));
 app.get("/", (req, res) => {
   console.log(path.join(__dirname, "../public"));
