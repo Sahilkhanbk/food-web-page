18e1a5c60757d25e65d74e0ef3a6b0875b28aadf
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-08-23 06:55:04 +0530

    Update index.ts

--- Food webpage/server/src/index.ts
+++ Food webpage/server/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
+
+
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
